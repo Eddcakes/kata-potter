cf82d680f2d73a5c1d665946d4b58ca63f34fb85
Eddcakes <eddadamson@gmail.com>
2024-09-25 22:34:19 +0100

    tests: update tests to be more readable and add edgecase

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,20 +1,17 @@
 import { describe, expect, it } from "vitest";
-import {
-  BasicBasket,
-  basicCalculateTotal,
-  calculateTotal,
-  formatPrice,
-} from "./utils";
+import { calculateTotal, formatPrice } from "./utils";
 import { Basket } from "./models";
 
 const baskets = [
   {
+    testCase: "one book",
     basket: {
       book1: { quantity: 1, price: 800, name: "Potter 1", sku: "book1" },
     },
     expected: 800,
   },
   {
+    testCase: "two unique books, multiple copies",
     basket: {
       book2: { quantity: 1, price: 800, name: "Potter 2", sku: "book2" },
       book3: { quantity: 3, price: 800, name: "Potter 3", sku: "book3" },
@@ -22,6 +19,7 @@ const baskets = [
     expected: 3120,
   }, // 2*8 * 0.95 + 2*8
   {
+    testCase: "several unique books, multiple copies",
     basket: {
       book1: { quantity: 2, price: 800, name: "Potter 1", sku: "book1" },
       book2: { quantity: 2, price: 800, name: "Potter 2", sku: "book2" },
@@ -31,6 +29,7 @@ const baskets = [
     expected: 4720,
   }, // 4*8 * 0.8  3* 8 * 0.9
   {
+    testCase: "lots of book",
     basket: {
       book1: { quantity: 5, price: 800, name: "Potter 1", sku: "book1" },
       book2: { quantity: 5, price: 800, name: "Potter 2", sku: "book2" },
@@ -41,6 +40,7 @@ const baskets = [
     expected: 14920,
   }, // 5*3 * 8 * 0.75 [90] 2*4 * 8 * 0.8 [51.2] + 8
   {
+    testCase: "one of each book",
     basket: {
       book1: { quantity: 1, price: 800, name: "Potter 1", sku: "book1" },
       book2: { quantity: 1, price: 800, name: "Potter 2", sku: "book2" },
@@ -51,52 +51,37 @@ const baskets = [
     expected: 3000,
   },
   {
+    testCase: "zero books",
     basket: {
       book1: { quantity: 0, price: 800, name: "Potter 1", sku: "book1" },
     },
     expected: 0,
   },
   {
+    testCase: "book with 0 quantity",
     basket: {
       book1: { quantity: 0, price: 800, name: "Potter 1", sku: "book1" },
       book4: { quantity: 2, price: 800, name: "Potter 4", sku: "book4" },
     },
     expected: 1600,
   },
-  // add more books
+  // found edge case
   {
+    testCase: "edge case with better than greedy price",
     basket: {
-      book1: { quantity: 1, price: 800, name: "Potter 1", sku: "book1" },
-      book2: { quantity: 1, price: 800, name: "Potter 2", sku: "book2" },
-      book3: { quantity: 1, price: 800, name: "Potter 3", sku: "book3" },
+      book1: { quantity: 2, price: 800, name: "Potter 1", sku: "book1" },
+      book2: { quantity: 2, price: 800, name: "Potter 2", sku: "book2" },
+      book3: { quantity: 2, price: 800, name: "Potter 3", sku: "book3" },
       book4: { quantity: 1, price: 800, name: "Potter 4", sku: "book4" },
       book5: { quantity: 1, price: 800, name: "Potter 5", sku: "book5" },
-      book6: { quantity: 1, price: 800, name: "Potter 6", sku: "book6" },
     },
-    expected: 3600,
-  },
-  {
-    basket: {
-      book2: {
-        sku: "book2",
-        name: "Potter 2",
-        price: 800,
-        quantity: 4,
-      },
-      book1: {
-        sku: "book1",
-        name: "Potter 1",
-        price: 800,
-        quantity: 3,
-      },
-    },
-    expected: 5360,
+    expected: 5120,
   },
 ];
 
 describe("calculateTotal", () => {
-  baskets.forEach(({ basket, expected }) => {
-    it(`calculates the total for ${JSON.stringify(basket)} correctly`, () => {
+  baskets.forEach(({ basket, expected, testCase }) => {
+    it(`calculates the total for ${testCase} correctly`, () => {
       expect(calculateTotal(basket as Basket)).toBe(expected);
     });
   });
@@ -115,30 +100,6 @@ describe("formatPrice", () => {
     it(`should format price for ${price}`, () => {
       const result = formatPrice(price as number);
       expect(result).toBe(expected);
-    });
-  });
-});
-
-const basicBaskets = [
-  { basket: { book1: 1 }, expected: 800 },
-  { basket: { book2: 1, book3: 3 }, expected: 3120 },
-  { basket: { book1: 2, book2: 2, book3: 2, book4: 1 }, expected: 4720 },
-  {
-    basket: { book1: 5, book2: 5, book3: 6, book4: 5, book5: 3 },
-    expected: 14920,
-  }, // 5*3 * 8 * 0.75 [90] 2*4 * 8 * 0.8 [51.2] + 8
-  {
-    basket: { book1: 2, book2: 2, book3: 2, book4: 1, book5: 1 },
-    expected: 5120,
-  },
-  { basket: { book1: 0 }, expected: 0 },
-  { basket: { book1: 0, book4: 2 }, expected: 1600 },
-];
-
-describe.only("basicCalculateTotal", () => {
-  basicBaskets.forEach(({ basket, expected }) => {
-    it(`calculates the total for ${JSON.stringify(basket)} correctly`, () => {
-      expect(basicCalculateTotal(basket as BasicBasket)).toBe(expected);
     });
   });
 });
